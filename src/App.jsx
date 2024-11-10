c6188b41530dfa0c4faf665b6b646b32e5a083e2
ajay1931 <ajayprakash032@gmail.com>
2024-11-10 13:16:36 +0530

    header and footer updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import SideBar from './Components/SideBar'
+import Header from './Components/Header'
+import Footer from './Components/Footer'
 
 function App() {
 
   return (
     <>
-      
+      <div className='app-container'>
+        <SideBar />
+        <div className='header-footer'>
+          <Header />
+          <Footer />
+        </div>
+      </div>
     </>
   )
 }
