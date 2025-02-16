bfddafae953f71d9468a928529aa21f1d8390d33
ajay1931 <ajayprakash032@gmail.com>
2025-02-16 16:35:27 +0530

    update app name

--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -17,7 +17,7 @@ const SideBar = () => {
         <div className="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark sidebar" style={{ width: "240px" }}>
             <Link to="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
                 <svg className="bi me-2" width="40" height="32"> <use href="#bootstrap" /></svg>
-                <span className="fs-4">Social Media</span>
+                <span className="fs-4">Link Up</span>
             </Link>
             <hr />
             <ul className="nav nav-pills flex-column mb-auto ">
